cde504ae6e86712ba551a3a6c9fb3375f8e6da38
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-01-08 09:19:06 -0800

    1/8/15

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -6,5 +6,25 @@ $("document").ready(function() {
   $(".1").css("background-color", "purple");  
   $(".1").css("font-style", "italic");
   $(".1").css("text-align", "center");
+   
+  $('h1').css("background-color", "green"); 
 });
 
+$("document").ready(function() {
+    $("body").css("background-color", "grey");
+    $("body").css("background-image", 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRdzVkKNy79CdtSbqBAm8WYSaGOtqgxyl-qslQknxsXFMs31IGw');
+    
+    });  
+    
+    
+    $("document").ready(function() {
+        $('#oneButton').bind('click', alertButtonClick);
+        
+    });
+    
+    function alertButtonClick(){
+        alert("there was a button clicked");
+    }
+    
+ $('body').css('background-image',"url(http://www.saidthegramophone.com/images/Deal_with_it_dog_gif.gif)");
+ $("body").css("background-size","cover");
