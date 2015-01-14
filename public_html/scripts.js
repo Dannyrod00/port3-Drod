6adf9f3a5a02905d6ac15679933e958641cf42a8
Daniel Rodriguez <dannyrodriguez822@gmail.com>
2015-01-14 09:19:35 -0800

    1/14/15

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -3,8 +3,8 @@
 
 
 $("document").ready(function() {
-  $("h1").css("font-style", "italic");
-  $(".1").css("text-align", "center");
+  $("h1").css("font-style", "Italic");
+  $("body").css("text-align", "center");
   $("p").css('color', 'purple');
   $('h1').css("background-color", "blue"); 
   $('h1').css('border', 'solid');
@@ -12,13 +12,32 @@ $("document").ready(function() {
 
 //these are my background color/image
 $("document").ready(function() {
-    $("body").css("background-color", " grey");
-    $('body').css('background-image',"url(http://www.saidthegramophone.com/images/Deal_with_it_dog_gif.gif)");
-       
+//    $("body").css("background-color", " grey");
+//    $('body').css('background-image',"url(http://www.saidthegramophone.com/images/Deal_with_it_dog_gif.gif)");
+//       
 });
 
 
+//$('#replaceWText').bind('click', replaceWText);
+//$('#randPara').bind('click', addAPara);
+//$('#').bind('click', );
 
+$('#show').bind('visibility', 'visible');
+
+$('#hide').bind('click', hideTheText);
+$('#show').bind('click', showTheText);
+
+
+
+function hideTheText(){
+  $('.h2').hide('slide', {}, 2500);
+  
+}
+
+function showTheText(){
+    $('.h2').show('fold', {}, 2500)
+    
+}
 
 
 
